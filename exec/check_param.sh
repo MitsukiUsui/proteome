c2b731ecfb935d7a945fd6fecc5e943b454d9dc7
Mitsuki <mitsuo.com.mu@gmail.com>
2017-05-14 00:06:25 +0900

    ADD check_param.sh

---
+++ exec/check_param.sh
@@ -0,0 +1,16 @@
+IFS=$'\n'
+#for line in `cat ../createcatalog/pnnl_downloads.csv`
+for line in `tail -n +2 ../createcatalog/head_pnnl_downloads.csv`
+do
+	organism=`echo ${line}|cut -d, -f 1`
+	dataset=`echo ${line}|cut -d, -f 3`
+	echo ${organism}
+	echo ${dataset}
+
+	mzidFilepath=/home/mitsuki/out/altorf/proteome/result/${organism}/${dataset}_annotated.mzid
+	paramDir=/home/mitsuki/out/altorf/proteome/param/${organism}/
+	myParamFilepath=${paramDir}${dataset}_annotated.param
+	../extractparam/extract_param.py ${mzidFilepath}>${myParamFilepath}
+	paramFilepath=${paramDir}${dataset}.param
+	diff -q ${myParamFilepath} ${paramFilepath} 
+done
